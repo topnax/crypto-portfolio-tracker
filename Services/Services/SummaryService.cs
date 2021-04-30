33a6f82cb237bf5be4f4ebb8527d0423488b3eaf
Stanislav Král <stanislav.kral3@gmail.com>
2021-04-30 07:28:15 +0200

    Added first implementation of the SummaryService

--- Services/Services/SummaryService.cs
+++ Services/Services/SummaryService.cs
@@ -1,20 +1,102 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
 using Model;
 
 namespace Services
 {
     public interface ISummaryService
     {
-        public record Summary(decimal AbsoluteProfit, decimal RelativeProfit, decimal MarketValue);
+        public record Summary(decimal AbsoluteChange, decimal RelativeChange, decimal MarketValue, decimal Cost);
 
         public Summary GetMarketOrderSummary(MarketOrder order, decimal assetPrice);
-        
-        public Summary GetPortfolioEntrySummary(PortfolioEntry entry, decimal assetPrice);
-        
-        public Summary GetPortfolioSummary(Portfolio entry, decimal assetPrice);
+
+        public Summary GetPortfolioEntrySummary(List<MarketOrder> portfolioEntryOrders, decimal assetPrice);
+
+        public Summary GetPortfolioSummary(Portfolio portfolio, Dictionary<string, decimal> assetPriceMap);
     }
-    
-    public class SummaryServiceImpl
+
+    public class SummaryServiceImpl : ISummaryService
     {
-        
+        public ISummaryService.Summary GetMarketOrderSummary(MarketOrder order, decimal assetPrice)
+        {
+            var marketValue = order.Size * assetPrice;
+            var cost = (order.Size * order.FilledPrice) + order.Fee;
+            var relativeChange = (marketValue / cost) - new decimal(1);
+            var absoluteChange = marketValue - cost;
+            return new(absoluteChange, relativeChange, marketValue, cost);
+        }
+
+        public ISummaryService.Summary GetAverageOfSummaries(IEnumerable<ISummaryService.Summary> summaries)
+        {
+            decimal totalMarketValue = 0;
+            decimal totalCost = 0;
+            decimal totalAbsoluteChange = 0;
+
+            // iterate over all orders and compute their summaries
+            foreach (var summary in summaries)
+            {
+                totalMarketValue += summary.MarketValue;
+                totalCost += summary.Cost;
+                totalAbsoluteChange += summary.AbsoluteChange;
+            }
+
+            decimal totalRelativeChange = (totalMarketValue / totalCost) / -1;
+
+            return new(totalAbsoluteChange, totalRelativeChange, totalMarketValue, totalCost);
+        }
+
+        public ISummaryService.Summary GetPortfolioEntrySummary(List<MarketOrder> portfolioEntryOrders, decimal assetPrice)
+        {
+            decimal totalHoldingSize = 0;
+            decimal totalSellValue = 0;
+            decimal totalCost = 0;
+            decimal totalFee = 0;
+            portfolioEntryOrders
+                .ForEach(order =>
+                {
+                    totalHoldingSize += order.Size * (order.Buy ? 1 : -1);
+                    var orderValue = order.Size * order.FilledPrice;
+                    if (!order.Buy)
+                    {
+                        totalSellValue += orderValue;
+                    }
+                    else
+                    {
+                        totalCost += orderValue;
+                    }
+
+                    totalFee += order.Fee;
+                });
+
+            decimal currentTotalHoldingValue = totalHoldingSize * assetPrice;
+
+            decimal totalAbsoluteChange = currentTotalHoldingValue + totalSellValue - totalCost - totalFee;
+            decimal totalRelativeChange = totalAbsoluteChange / totalCost;
+
+            return new ISummaryService.Summary(totalAbsoluteChange, totalRelativeChange, currentTotalHoldingValue,
+                totalCost + totalFee);
+        }
+
+        public ISummaryService.Summary GetPortfolioSummary(Portfolio portfolio,
+            Dictionary<string, decimal> assetPriceMap)
+        {
+            return null;
+            // var portfolioEntrySumarries = PortfolioEntryService.GetPortfolioEntries(portfolio.Id).Select(entry =>
+            // {
+            //     if (!assetPriceMap.TryGetValue(entry.Symbol, out var assetPrice))
+            //     {
+            //         throw new AssetPriceNotFoundException();
+            //     }
+
+            //     return GetPortfolioEntrySummary(entry, assetPrice);
+            // });
+            // return GetAverageOfSummaries(portfolioEntrySumarries);
+        }
+
+        public class AssetPriceNotFoundException : Exception
+        {
+        }
     }
 }
