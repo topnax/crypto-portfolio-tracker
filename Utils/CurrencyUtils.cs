def3587649674bf1b50dd0f76c5f262a3eafcd0b
Stanislav Král <stanislav.kral3@gmail.com>
2021-05-21 07:48:18 +0200

    Added documentation to CurrencyUtils.cs

--- Utils/CurrencyUtils.cs
+++ Utils/CurrencyUtils.cs
@@ -7,6 +7,11 @@ namespace Utils
 {
     public class CurrencyUtils
     {
+        /// <summary>
+        /// Returns a label of the given currency
+        /// </summary>
+        /// <param name="currency">Currency whose label should be returned</param>
+        /// <returns>Label of the given currency</returns>
         public static string GetCurrencyLabel(Currency currency)
         {
             switch (currency)
@@ -22,6 +27,12 @@ namespace Utils
             return "UNDEFINED";
         }
 
+        /// <summary>
+        /// Format's the given value and currency due to currency's formatting rules
+        /// </summary>
+        /// <param name="value">Value to be formatted</param>
+        /// <param name="currency">Currency to be used</param>
+        /// <returns>A string containing both the value and the currency symbol in the correct format</returns>
         public static string Format(decimal value, Currency currency)
         {
             var valueStr = DecimalUtils.FormatTwoDecimalPlaces(Math.Abs(value));
@@ -50,6 +61,13 @@ namespace Utils
             return output;
         }
 
+        /// <summary>
+        /// Format's the given value and currency due to currency's formatting rules. Adds the plus symbol when the value
+        /// is positive.
+        /// </summary>
+        /// <param name="value">Value to be formatted</param>
+        /// <param name="currency">Currency to be used</param>
+        /// <returns>A string containing both the value and the currency symbol in the correct format</returns>
         public static string FormatWithPlusSign(decimal value, Currency currency) =>
             (value > 0 ? "+" : "") + Format(value, currency);
     }
