d0df169739ea911d9ab5f5be005001a3d1852e5e
Stanislav Král <stanislav.kral3@gmail.com>
2021-05-21 07:50:24 +0200

    Added documentation to CurrencyUtils.cs

--- Utils/DecimalUtils.cs
+++ Utils/DecimalUtils.cs
@@ -5,17 +5,20 @@ namespace Utils
 {
     public static class DecimalUtils
     {
-        private static NumberFormatInfo whitespaceSeparatorNfi = (NumberFormatInfo)CultureInfo.InvariantCulture.NumberFormat.Clone();
+        // define white space separator number format, it is to be used in all format methods on this class
+        private static readonly NumberFormatInfo WhitespaceSeparatorNf = (NumberFormatInfo)CultureInfo.InvariantCulture.NumberFormat.Clone();
+        
         static DecimalUtils()
         {
-            whitespaceSeparatorNfi.NumberGroupSeparator = " ";
+            // make the number format use whitespace as thousands separator
+            WhitespaceSeparatorNf.NumberGroupSeparator = " ";
         }
         
-        public static string FormatTrimZeros(decimal value) => value.ToString("#,0.#############", whitespaceSeparatorNfi);
+        public static string FormatTrimZeros(decimal value) => value.ToString("#,0.#############", WhitespaceSeparatorNf);
         
-        public static string FormatTwoDecimalPlaces(decimal value) => value.ToString("#,0.00", whitespaceSeparatorNfi);
+        public static string FormatTwoDecimalPlaces(decimal value) => value.ToString("#,0.00", WhitespaceSeparatorNf);
         
-        public static string FormatFiveDecimalPlaces(decimal value) => value.ToString("#,0.00000", whitespaceSeparatorNfi);
+        public static string FormatFiveDecimalPlaces(decimal value) => value.ToString("#,0.00000", WhitespaceSeparatorNf);
 
         public static string FormatTwoDecimalPlacesWithPlusSign(decimal value) =>
             (value > 0 ? "+" : "") + FormatTwoDecimalPlaces(value);
