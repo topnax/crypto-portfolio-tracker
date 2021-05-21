d8a7423c4ee84ca5d4f357a8dfdbecb92d1b2b4f
Stanislav Král <stanislav.kral3@gmail.com>
2021-05-21 07:57:59 +0200

    Added documentation to EnumUtils.cs

--- Utils/EnumUtils.cs
+++ Utils/EnumUtils.cs
@@ -4,8 +4,13 @@ using System.Linq;
 
 namespace Utils
 {
-    public class EnumUtils
+    public static class EnumUtils
     {
+        /// <summary>
+        /// Returns all possible values of the given enumerable
+        /// </summary>
+        /// <typeparam name="TEnum">Enumerable whose values are to be returned</typeparam>
+        /// <returns>List of possible values of the given enumerable</returns>
         public static List<TEnum> GetEnumList<TEnum>() where TEnum : Enum 
             => ((TEnum[])Enum.GetValues(typeof(TEnum))).ToList();
     }
